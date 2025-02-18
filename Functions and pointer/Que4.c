4bd3272506e1389d49fce98b88da1c4a61203d20
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-02-18 14:54:28 +0530

    Que

---
+++ Functions and pointer/Que4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int sum(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    int a, b;
+    printf("Enter the value of a :");
+    scanf("%d", &a);
+    printf("Enter the value of b :");
+    scanf("%d", &b);
+    int s = sum(a, b);
+    printf("sum : %d", s);
+    return 0;
+}
