9beac31116633d00d4e68de9fa6962840a1b9e3b
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-02-21 12:36:37 +0530

    Function Prototype

---
+++ Functions and pointer/fn_prototype.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void greet();  // Function prototype
+
+int main() {
+    greet();  // Function call
+    return 0;
+}
+
+void greet() {  // Function definition
+    printf("Hello, World!\n");
+}
